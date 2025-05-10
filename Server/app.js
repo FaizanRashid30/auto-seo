d563f2f2c5cdeb189d224ebd7a27ce8b4f8f642a
Faizan <faizanrashid1863@gmail.com>
2025-05-10 09:29:59 +0500

    server

--- Server/app.js
+++ Server/app.js
@@ -1,66 +1,90 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const cors =require("cors");
-require("./db/conn.js")
+const cors = require("cors");
+require("./db/conn.js");
 const PORT = 6005;
-const session= require("express-session");
+const session = require("express-session");
 const passport = require("passport");
 const OAuth2Strategy = require("passport-google-oauth2").Strategy;
-const userdb =require("../model/userSchema.js")
+const userdb = require("./model/userSchema.js");
 
+const clientId =
+  "1018770991494-q179e9gn2ee0ffid9pkbtc3epfvddjb4.apps.googleusercontent.com";
+const clientSecret = "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";
 
-const clientId="1018770991494-q179e9gn2ee0ffid9pkbtc3epfvddjb4.apps.googleusercontent.com"
-const clientSecret="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"
- 
-app.use(cors({
-    origin:"http://localhost:5173",
-    methods:"GET, POST, PUT, DELETE",
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-    
-app.use(session({
-    secret:"12121212ppkk",
+
+app.use(
+  session({
+    secret: "12121212ppkk",
     resave: false,
-    saveUninitialized:true,
-}))
+    saveUninitialized: true,
+  })
+);
 
 app.use(passport.initialize());
-app.use(passport.session()); 
+app.use(passport.session());
 
 passport.use(
-    new OAuth2Strategy({
-        clientID:clientid,
-        clientSecret:clientsecret,
-        callbackURL:"/auth/google/callback",
-        scope:["profile","email"]
-    },async(accessToken, refreshToken, profile, done)=>{
-        try{
-            let user = await userdb.findOne({googleId:profile.id});
-            if(!user){
-                user= new userdb({
-                    googleId:profile.id,
-                    displayName:profile.displayName,
-                    email:profile.emails[0].value,
-                    image:profile.photos[0].value
-                });
+  new OAuth2Strategy(
+    {
+      clientID: clientId,
+      clientSecret: clientSecret,
+      callbackURL: "/auth/google/callback",
+      scope: ["profile", "email"],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await userdb.findOne({ googleId: profile.id });
+        if (!user) {
+          user = new userdb({
+            googleId: profile.id,
+            displayName: profile.displayName,
+            email: profile.emails[0].value,
+            image: profile.photos[0].value,
+          });
 
-                await user.save();
-            }
-            return done(null, user)
-        }catch(error){
-            return done(error, null)
+          await user.save();
         }
-    })
-)
+        return done(null, user);
+      } catch (error) {
+        return done(error, null);
+      }
+    }
+  )
+);
 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
-app.get("/", (req,res)=>{
-    res.status(200).json("server start")
-})
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "http://localhost:5173/",
+    failureRedirect: "http://localhost:5173/login",
+  })
+);
+// app.get("/", (req,res)=>{
+//     res.status(200).json("server start")
+// })
 
-app.listen(PORT, ()=>{
-    console.log(`server start at port no ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server start at port no ${PORT}`);
+});
